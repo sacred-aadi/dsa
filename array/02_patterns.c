65deb6b0f5e96a3607a2412e164d63ba28d5101c
sacred.aadi <amheisenbergsprinciple@gmail.com>
2026-07-23 18:41:49 +0530

    pattern with increasing the integer

--- array/02_patterns.c
+++ array/02_patterns.c
@@ -8,4 +8,15 @@ int main (){
         }
         printf("\n");
     }
+    //basic code for pattern printing in C language. This code prints a rectangle of asterisks with 5 rows and 7 columns.
+    
+    // creating a triangle where the int increaases
+    int r ;
+    scanf("%d",&r);
+    for(int i=1;i<r+1;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+    }
 }
